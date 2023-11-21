08cfd0c628f600dfa3f53e9b7180c44db61f8986
dungnn <ngocdungbkhn@gmail.com>
2023-11-21 23:29:14 +0700

    issue1_login logout

---
+++ build/CMakeFiles/engine.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/engine.dir/src/account.cpp.o"
+  "CMakeFiles/engine.dir/src/example.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
